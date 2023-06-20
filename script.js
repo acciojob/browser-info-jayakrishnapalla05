990b2c5e1dd7bd241d7bd304a499a9496a4aecd5
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-20 14:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const div=document.getElementById('Browser-info');
+const div=document.getElementById('browser-info');
 const bName=navigator.appName;
 const version=navigator.appVersion;
 div.innerText="You are using ${bName} version ${version}";
